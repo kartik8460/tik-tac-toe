5e636d7429db726b027bbdf42f7f2c1bc64691ab
kartik8460 <umeshgupta700@gmail.com>
2020-07-05 12:42:58 +0530

    created Component icon and installed bootstrap and ngx-toaster (also entry in package.json)

---
+++ src/app/components/iconny/iconny.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iconny',
+  templateUrl: './iconny.component.html',
+  styleUrls: ['./iconny.component.css']
+})
+export class IconnyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
