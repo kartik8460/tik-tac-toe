94b15111250b9cc7674654f87740368e526d1505
kartik8460 <umeshgupta700@gmail.com>
2020-07-05 12:45:09 +0530

    deleted iconny component (created instead of icon component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IconnyComponent } from './components/iconny/iconny.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IconnyComponent
   ],
   imports: [
     BrowserModule,
